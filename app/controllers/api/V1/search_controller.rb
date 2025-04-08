e30ab2385a4fb9edc0997ade903ea59fe24e108f
Abubakar <Haywayaheadshotstrategy.fx@gmail.com>
2025-04-08 22:57:21 +0000

    revert changes

--- app/controllers/api/V1/search_controller.rb
+++ app/controllers/api/V1/search_controller.rb
@@ -12,29 +12,6 @@ module Api
         render json: { error: 'Search failed. Please try again later.' },
                status: :internal_server_error
       end
-
-      private
-
-      def validate_query
-        return unless query.blank? || query.length < 3
-
-        render json: { error: 'Invalid query. Query must be at least 3 characters long.' },
-               status: :bad_request and return
-      end
-
-      def query
-        params[:q]
-      end
-
-      def validate_ip
-        return if params[:ip].present?
-
-        render json: { error: 'IP address is required' }, status: :bad_request and return
-      end
-
-      def ip
-        params[:ip]
-      end
     end
   end
 end
