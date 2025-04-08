892dca405c60ca7676b7832dc19e974d48a0cf24
Abubakar <Haywayaheadshotstrategy.fx@gmail.com>
2025-04-08 22:41:16 +0000

    adjust base controller

--- app/controllers/api/V1/analytics_controller.rb
+++ app/controllers/api/V1/analytics_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    class AnalyticsController < BaseController
+    class AnalyticsController < ApplicationController
       before_action :validate_ip, only: %i[index update]
       before_action :validate_query, only: :update
 
@@ -44,6 +44,29 @@ module Api
         else
           render json: { error: 'No previous search found for this IP' }, status: :not_found
         end
+      end
+
+      private
+
+      def validate_query
+        return unless query.blank? || query.length < 3
+
+        render json: { error: 'Invalid query. Query must be at least 3 characters long.' },
+               status: :bad_request and return
+      end
+
+      def query
+        params[:q]
+      end
+
+      def validate_ip
+        return if params[:ip].present?
+
+        render json: { error: 'IP address is required' }, status: :bad_request and return
+      end
+
+      def ip
+        params[:ip]
       end
     end
   end
--- app/controllers/api/V1/search_controller.rb
+++ app/controllers/api/V1/search_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    class SearchController < BaseController
+    class SearchController < ApplicationController
       before_action :validate_query, only: :create
       before_action :validate_ip, only: :create
 
@@ -11,6 +11,26 @@ module Api
         Rails.logger.error("Search error: #{e.message}")
         render json: { error: 'Search failed. Please try again later.' },
                status: :internal_server_error
+      end
+
+      private
+
+      def validate_query
+ 
+      end
+
+      def query
+        params[:q]
+      end
+
+      def validate_ip
+        return if params[:ip].present?
+
+        render json: { error: 'IP address is required' }, status: :bad_request and return
+      end
+
+      def ip
+        params[:ip]
       end
     end
   end
